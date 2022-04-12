6fde3e14a828faf8f6e3bb3a40528c3c55fceb61
FedericoCapitani <federico.capitani@outlook.it>
2022-04-12 15:43:06 +0200

    aggiunte immagini e inizio creazione cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,37 +13,50 @@ Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i 
 
 /* Milestone 3
 Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like. */
-
-
+/**
+ * 
+ * @param {object} array di oggeti per creare le card
+ */
+function createPosts(array){
+    array.forEach((element) => {
+        let post = document.querySelector('.container')
+        let div = document.createElement('div')
+        let pfp = document.createElement('div')
+        let author = document.createElement('div')
+        let date = document.createElement('div')
+        let post_text = document.createElement('div')
+        div.classList.add('card')
+        pfp.classList.add('profile_picture')
+        pfp.innerHTML = `<img src="${element.foto_autore}" alt="">`
+        author.classList.add('autore')
+        author.innerHTML = `${element.nome_autore} <br> ${element.data}`
+        post_text.innerHTML = `${element.testo_post}`
+        
+        div.append(pfp, author, date, post_text)
+        post.append(div)
+    })
+}
 
 const posts = 
 [
     {
         id: 1,
         nome_autore: 'Marco Rossi',
-        foto_autore: '',
+        foto_autore: './assets/img/pfp-1.jpg',
         data: '06/23/2021',
         testo_post: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum assumenda corrupti facilis sapiente soluta veritatis asperiores itaque, sequi distinctio culpa!',
-        immagine: 'https://picsum.photos/200',
+        immagine: './assets/img/post-img-1.jpg',
         numero_likes: 67
     },
     {
         id: 2,
         nome_autore: 'Laura Pascal',
-        foto_autore: '',
+        foto_autore: './assets/img/pfp-2.jpg',
         data: '08/15/2020',
         testo_post: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum assumenda corrupti facilis sapiente soluta veritatis asperiores itaque, sequi distinctio culpa!',
-        immagine: 'https://picsum.photos/200',
-        numero_likes: 245
-    },
-    {
-        id: 3,
-        nome_autore: 'Sara Violini',
-        foto_autore: '',
-        data: '02/01/2022',
-        testo_post: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum assumenda corrupti facilis sapiente soluta veritatis asperiores itaque, sequi distinctio culpa!',
-        immagine: 'https://picsum.photos/200',
+        immagine: './assets/img/post-img-2.jpg',
         numero_likes: 173
     }
 ]
 
+createPosts(posts);
