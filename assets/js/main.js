424acec6470eacac5be1fac92cbebe2f67131723
FedericoCapitani <federico.capitani@outlook.it>
2022-04-12 14:04:51 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+/* Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes. */
+
+const posts = 
+[
+    {
+        id: 1,
+        nome_autore: 'Marco Rossi',
+        foto_autore: '',
+        data: '06/23/2021',
+        testo_post: '',
+        immagine: '',
+        numero_likes: 67
+    },
+    {
+        id: 2,
+        nome_autore: 'Laura Pascal',
+        foto_autore: '',
+        data: '08/15/2020',
+        testo_post: '',
+        immagine: '',
+        numero_likes: 245
+    },
+    {
+        id: 3,
+        nome_autore: 'Sara Violini',
+        foto_autore: '',
+        data: '02/01/2022',
+        testo_post: '',
+        immagine: '',
+        numero_likes: 173
+    },
+    {
+        id: 4,
+        nome_autore: 'Luca Marini',
+        foto_autore: '',
+        data: '11/27/2021',
+        testo_post: '',
+        immagine: '',
+        numero_likes: 104
+    },
+]
